27a43d7a9d81aff0c27fe2104442a4f0b12eb813
Betzaida Taylor <156623909+Betzaida96@users.noreply.github.com>
2024-05-12 18:22:08 -0600

    debugging readme.js

--- readme.md
+++ readme.md
@@ -1,23 +1,29 @@
 # hi
-    [!Gitbub license] (https://img.shields.io/badge/licenseBSD 3-blue)
-    # Description
-    hi
-    
-    # Table of Contents
-    * [Installation](#installation)
-    * [Tests](#tests)
-    * [Usage](#usage)
-    * [Contributions](#contributions)
-    * [Questions](#questions)
-    
-    # Installation
-    hi
-    # Tests
-    hi
-    # Usage
-    hi
-    # Contributions
-    hi
-    # Questions
-    If you have any questions, feel free to reach out to hi or find me on GitHub at hi.
-    
+[![Gitbub license] (https://img.shields.io/badge/license/APACHE 2.0-blue)]
+
+## Description
+waetf
+
+
+## Table of Contents
+* [Installation](#installation)
+* [Tests](#tests)
+* [Usage](#usage)
+* [Contributions](#contributions)
+* [Questions](#questions)
+
+
+## Installation
+eaf
+
+## Tests
+weag
+
+## Usage
+reafgv
+
+## Contributions
+wef
+
+## Questions
+If you have any questions, feel free to reach out to aega or find me on GitHub at i.
--- utils/readme.js
+++ utils/readme.js
@@ -1,7 +1,7 @@
 //create a function that returns a licnese badge from the license you pick
 function renderLicensesBadge(licenses) {
     if (licenses !== "none") {
-        return `[!Gitbub license] (https://img.shields.io/badge/license${licenses}-blue)`;
+        return `[![Gitbub license] (https://img.shields.io/badge/license/${licenses}-blue)]`;
     }
     return "";
 }
@@ -10,30 +10,39 @@ function renderLicensesBadge(licenses) {
 function generateREADME(data) {
     const licenseBadge = renderLicensesBadge(data.licenses);
     const tableOfContents = `
-    # Table of Contents
-    * [Installation](#installation)
-    * [Tests](#tests)
-    * [Usage](#usage)
-    * [Contributions](#contributions)
-    * [Questions](#questions)
-    `;
+## Table of Contents
+* [Installation](#installation)
+* [Tests](#tests)
+* [Usage](#usage)
+* [Contributions](#contributions)
+* [Questions](#questions)
+`;
+
     return `# ${data.project}
-    ${licenseBadge}
-    # Description
-    ${data.description}
-    ${tableOfContents}
-    # Installation
-    ${data.installation}
-    # Tests
-    ${data.tests}
-    # Usage
-    ${data.usage}
-    # Contributions
-    ${data.contributions}
-    # Questions
-    If you have any questions, feel free to reach out to ${data.email} or find me on GitHub at ${data.username}.
-    `;
+${licenseBadge}
+
+## Description
+${data.description}
+
+${tableOfContents}
+
+## Installation
+${data.installation}
+
+## Tests
+${data.tests}
+
+## Usage
+${data.usage}
+
+## Contributions
+${data.contributions}
+
+## Questions
+If you have any questions, feel free to reach out to ${data.email} or find me on GitHub at ${data.username}.
+`;
 }
+
 
 
 module.exports = generateREADME;
